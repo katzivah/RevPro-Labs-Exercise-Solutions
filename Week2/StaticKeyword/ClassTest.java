bde3a9d48c0fffd6d54c323293f1019dfb853cc8
Karis Caruso <cchristophercmc@gmail.com>
2025-10-04 13:33:50 -0400

    Create ClassTest.java

---
+++ Week2/StaticKeyword/ClassTest.java
@@ -0,0 +1,13 @@
+public class ClassTest {
+    Runner r = new Runner();
+
+    /**
+     * After running changeClassVar("xyz"), the value of global_var should be "xyz".
+     */
+    @Test
+    public void classTest1(){
+        Class.global_var="abc";
+        r.changeClassVar("xyz");
+        Assert.assertEquals("xyz", Class.global_var);
+    }
+}
